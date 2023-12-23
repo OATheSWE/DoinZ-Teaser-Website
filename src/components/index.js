8873bba3ffe7dfbef9882d4ed7ea70b750fe79a4
OATheSWE <osborneosas12@gmail.com>
2023-12-24 00:38:35 +0100

    Made a minor change

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Btn } from "./Button";
 export { default as ScrollToTopBtn } from "./ScrollToTopBtn";
 export * from "./NavBar"
 export * from "./Header"
-export * from "./product"
+export * from "./Product"
 export * from "./Launch"
 export * from "./Contact"
 export * from "./About"
